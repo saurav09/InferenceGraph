825a3b3fc91e879434782046017c0017795090f0
saurav <saurav.raj@paypal.com>
2022-10-31 20:54:23 +0530

    fix routing to downstream services

--- Router/Router.py
+++ Router/Router.py
@@ -6,27 +6,7 @@ logger = get_logger()
 class Router:
     def __init__(self):
         logger.info("Initializing router service")
-        print("Initializing router service")
 
     def route(self, features, feature_names=None) -> int:
         logger.info(f"Request in Router is {features}")
-        print(f"Request in Router is {features}")
-        return -1
-
-
-if __name__ == '__main__':
-    request_all_fields = [{
-        "source_language": "en",
-        "target_language": "hi",
-        "translation_items": [
-            {
-                "item_id": "1",
-                "text": "English is a language"
-            },
-            {
-                "item_id": "2",
-                "text": "We will extend the conventions laid out to accommodate this for sure."
-            },
-        ]
-    }]
-    print(Router().route(request_all_fields))
+        return 0
